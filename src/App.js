64a61bc48341046abc3eb16ec45ed6d116c72887
Oleh Andreiev <oandreiev@lohika.com>
2020-09-20 13:10:33 +0300

    added glider implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
-import SimpleExample from "./tasks/class-works/cw1/SimpleExample";
 import Week1 from "./tasks/home-works/dz1/Week1";
 import CompositionExample from "./tasks/class-works/cw2/composition/CompositionExample";
 import Blog from "./tasks/class-works/cw2/life-cycle-hooks/Blog";
 import AutofocusInput from "./tasks/class-works/cw2/refs/refs-example";
+import Dz2 from "./tasks/home-works/dz2/Dz2";
 
 
 class App extends Component {
 
   render() {
     return (
-      <>
-        <AutofocusInput />
-        <Blog />
-        <CompositionExample />
-        <Week1 />
-      </>
+      <Dz2 />
     )
   }
 }
